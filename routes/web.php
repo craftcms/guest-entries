897a5057349811a80e91fcb954b87e93e23bf472
August Miller <hello@gusmiller.com>
2026-04-29 16:11:04 -0700

    Pint :)

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use CraftCms\GuestEntries\Http\Controllers\CreateGuestEntryController;
 use CraftCms\GuestEntries\Plugin;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['throttle:guest-entries'])->group(function() {
-    /** @var CraftCms\GuestEntries\Settings $settings */
+Route::middleware(['throttle:guest-entries'])->group(function () {
+    /** @var \CraftCms\GuestEntries\Settings $settings */
     $settings = Plugin::getInstance()->getSettings();
 
     Route::post($settings->endpoint, CreateGuestEntryController::class);
